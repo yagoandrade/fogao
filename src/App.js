23ca3b1e876cac50feb0f6a94d731ea3b67feded
yagoandrade <ayav@ic.ufal.br>
2022-04-27 16:26:48 -0300

    Revert

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
+import todosDesligados from "./images/todos-desligados.jpeg";
+import um from "./images/1-ligado.jpeg";
 import umDois from "./images/1-2-ligado.jpeg";
 import umDoisTres from "./images/1-2-3-ligado.jpeg";
 import umDoisQuatro from "./images/1-2-4-ligado.jpeg";
